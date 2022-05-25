5e06ce2177343345ce5501cf12fb2a24e3e59ab5
maxence <maxence.cornaton@iex.ec>
2022-05-25 16:15:08 +0200

    Remove values out of specs

--- src/test/java/com/iexec/worker/tee/post/compute/MainTests.java
+++ src/test/java/com/iexec/worker/tee/post/compute/MainTests.java
@@ -18,7 +18,7 @@ class MainTests {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+    @ValueSource(ints = {0, 1, 2, 3})
     void shouldExitWithCode(int expectedExitCode) throws Exception {
         final PostComputeAppRunner postComputeAppRunner = mock(PostComputeAppRunner.class);
         when(postComputeAppRunner.start()).thenReturn(expectedExitCode);
