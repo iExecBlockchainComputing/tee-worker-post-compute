f4705ea540492a0f26c3fd7fed74e5ff6e51e5b0
Maxence Cornaton <maxence.cornaton@iex.ec>
2023-03-03 10:39:15 +0100

    Add UTs

--- src/main/java/com/iexec/worker/tee/post/compute/worker/WorkerApiManager.java
+++ src/main/java/com/iexec/worker/tee/post/compute/worker/WorkerApiManager.java
@@ -22,10 +22,18 @@ import feign.Logger;
 import org.apache.commons.lang3.StringUtils;
 
 public class WorkerApiManager {
+    public static final String DEFAULT_WORKER_HOST = "worker:13100";
     private static final String WORKER_HOST_ENV_VAR = "WORKER_HOST";
     private static final String WORKER_HOST = getWorkerHost();
 
     private static WorkerApiClient workerApiClient;
+
+    public static String getWorkerHost() {
+        final String host = EnvUtils.getEnvVar(WORKER_HOST_ENV_VAR);
+        return StringUtils.isBlank(host)
+                ? DEFAULT_WORKER_HOST
+                : host;
+    }
 
     private WorkerApiManager() {
         throw new UnsupportedOperationException();
@@ -38,12 +46,5 @@ public class WorkerApiManager {
                     .target(WorkerApiClient.class, "http://" + WORKER_HOST);
         }
         return workerApiClient;
-    }
-
-    public static String getWorkerHost() {
-        final String host = EnvUtils.getEnvVar(WORKER_HOST_ENV_VAR);
-        return StringUtils.isBlank(host)
-                ? "worker:13100"
-                : host;
     }
 }
--- src/test/java/com/iexec/worker/tee/post/compute/worker/WorkerApiManagerTests.java
+++ src/test/java/com/iexec/worker/tee/post/compute/worker/WorkerApiManagerTests.java
@@ -1,6 +1,9 @@
 package com.iexec.worker.tee.post.compute.worker;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -9,7 +12,26 @@ import java.lang.reflect.InvocationTargetException;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
+@ExtendWith(SystemStubsExtension.class)
 class WorkerApiManagerTests {
+    // region getWorkerHost
+    @Test
+    void shouldGetWorkerHostFromEnvVar(EnvironmentVariables variables) {
+        final String workerHost = "host:1234";
+        variables.set("WORKER_HOST", workerHost);
+
+        final String result = WorkerApiManager.getWorkerHost();
+        assertSame(workerHost, result);
+    }
+
+    @Test
+    void shouldUseDefaultWorkerHost() {
+        final String result = WorkerApiManager.getWorkerHost();
+        assertSame("worker:13100", result);
+    }
+    // endregion
+
+    // region Constructor
     @Test
     void shouldNotCreateNewInstance() throws NoSuchMethodException {
         final Constructor<WorkerApiManager> constructor = WorkerApiManager.class.getDeclaredConstructor();
@@ -18,7 +40,9 @@ class WorkerApiManagerTests {
         final InvocationTargetException exception = assertThrows(InvocationTargetException.class, constructor::newInstance);
         assertTrue(exception.getTargetException() instanceof UnsupportedOperationException);
     }
+    // endregion
 
+    // region getWorkerApiClient
     @Test
     void shouldGetExistingWorkerApiClient() throws NoSuchFieldException, IllegalAccessException {
         final WorkerApiClient workerApiClient = mock(WorkerApiClient.class);
@@ -40,4 +64,5 @@ class WorkerApiManagerTests {
         final WorkerApiClient workerApiClient = WorkerApiManager.getWorkerApiClient();
         assertNotNull(workerApiClient);
     }
+    // endregion
 }
