c10ef8ae66c0b43eb6e908325d9b081d49c477c0
nabil-Tounarti <nabil.tounarti0@gmail.com>
2025-03-03 13:04:07 +0100

    Resolve SonarQube issues

--- src/test/java/com/iexec/worker/compute/post/signer/SignerServiceTests.java
+++ src/test/java/com/iexec/worker/compute/post/signer/SignerServiceTests.java
@@ -37,19 +37,19 @@ import static org.mockito.Mockito.when;
 
 class SignerServiceTests {
 
-    private static final String chainTaskId = "0x123456789abcdef";
-    private static final String workerAddress = "0xabcdef123456789";
-    private static final String enclaveChallengePrivateKey = "0xdd3b993ec21c71c1f6d63a5240850e0d4d8dd83ff70d29e49247958548c1d479";
-    private static final String messageHash = "0x5cd0e9c5180dd35e2b8285d0db4ded193a9b4be6fbfab90cbadccecab130acad";
-    private static final String expectedChallenge = "0xfcc6bce5eb04284c2eb1ed14405b943574343b1abda33628fbf94a374b18dd16541c6ebf63c6943d8643ff03c7aa17f1cb17b0a8d297d0fd95fc914bdd0e85f81b";
+    private static final String CHAIN_TASK_ID = "0x123456789abcdef";
+    private static final String WORKER_ADDRESS = "0xabcdef123456789";
+    private static final String ENCLAVE_CHALLENGE_PRIVATE_KEY = "0xdd3b993ec21c71c1f6d63a5240850e0d4d8dd83ff70d29e49247958548c1d479";
+    private static final String MESSAGE_HASH = "0x5cd0e9c5180dd35e2b8285d0db4ded193a9b4be6fbfab90cbadccecab130acad";
+    private static final String EXPECTED_CHALLENGE = "0xfcc6bce5eb04284c2eb1ed14405b943574343b1abda33628fbf94a374b18dd16541c6ebf63c6943d8643ff03c7aa17f1cb17b0a8d297d0fd95fc914bdd0e85f81b";
 
     @Spy
     SignerService signerService = new SignerService();
 
     @Test
     void shouldSignEnclaveChallenge() {
-        final String actualChallenge = Assertions.assertDoesNotThrow(() -> signerService.signEnclaveChallenge(messageHash, enclaveChallengePrivateKey));
-        assertEquals(expectedChallenge, actualChallenge);
+        final String actualChallenge = Assertions.assertDoesNotThrow(() -> signerService.signEnclaveChallenge(MESSAGE_HASH, ENCLAVE_CHALLENGE_PRIVATE_KEY));
+        assertEquals(EXPECTED_CHALLENGE, actualChallenge);
     }
 
     @Test
@@ -58,7 +58,7 @@ class SignerServiceTests {
             signatureUtils.when(() -> SignatureUtils.isExpectedSignerOnSignedMessageHash(any(), any(), any()))
                     .thenReturn(false);
 
-            final PostComputeException exception = assertThrows(PostComputeException.class, () -> signerService.signEnclaveChallenge(messageHash, enclaveChallengePrivateKey));
+            final PostComputeException exception = assertThrows(PostComputeException.class, () -> signerService.signEnclaveChallenge(MESSAGE_HASH, ENCLAVE_CHALLENGE_PRIVATE_KEY));
             assertEquals(POST_COMPUTE_INVALID_TEE_SIGNATURE, exception.getStatusCause());
             assertEquals("Failed to verify TeeEnclaveChallenge signature (exiting)", exception.getMessage());
         }
@@ -72,17 +72,17 @@ class SignerServiceTests {
              MockedStatic<HashUtils> hashUtils = Mockito.mockStatic(HashUtils.class)) {
 
             envUtils.when(() -> EnvUtils.getEnvVarOrThrow(eq(RESULT_SIGN_WORKER_ADDRESS), any()))
-                    .thenReturn(workerAddress);
+                    .thenReturn(WORKER_ADDRESS);
             envUtils.when(() -> EnvUtils.getEnvVarOrThrow(eq(RESULT_SIGN_TEE_CHALLENGE_PRIVATE_KEY), any()))
-                    .thenReturn(enclaveChallengePrivateKey);
-            hashUtils.when(() -> HashUtils.concatenateAndHash(chainTaskId, workerAddress))
-                    .thenReturn(messageHash);
+                    .thenReturn(ENCLAVE_CHALLENGE_PRIVATE_KEY);
+            hashUtils.when(() -> HashUtils.concatenateAndHash(CHAIN_TASK_ID, WORKER_ADDRESS))
+                    .thenReturn(MESSAGE_HASH);
 
-            when(mockSignerService.signEnclaveChallenge(messageHash, enclaveChallengePrivateKey))
-                    .thenReturn(expectedChallenge);
+            when(mockSignerService.signEnclaveChallenge(MESSAGE_HASH, ENCLAVE_CHALLENGE_PRIVATE_KEY))
+                    .thenReturn(EXPECTED_CHALLENGE);
 
-            String actualChallenge = mockSignerService.getChallenge(chainTaskId);
-            assertEquals(expectedChallenge, actualChallenge);
+            String actualChallenge = mockSignerService.getChallenge(CHAIN_TASK_ID);
+            assertEquals(EXPECTED_CHALLENGE, actualChallenge);
         }
     }
 
